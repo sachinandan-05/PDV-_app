9949665fd6e02abcd71b00aaecd09c60b10216d3
Sachinandan <112381035+sachinandan-05@users.noreply.github.com>
2025-03-22 15:05:47 +0530

    Update Nav.tsx

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -7,7 +7,7 @@ const Nav:React.FC = () => {
       <nav className='w-full bg-slate-100  border-1 border-black h-18   shadow shadow-slate-500 z-10  '> 
         <div className='w-full flex justify-between'>
         <div className=' font-bold p-5 font-serif text-[24px] cursor-pointer ' onClick={()=>navigate("/")}>DensityVista</div>
-        <div className='p-5 font-thin'>By<span className=' cursor-pointer font-roboto text-blue-400'><a href="https://www.linkedin.com/in/sachinandan-yadav-660115243?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BvFBdNA6RRMiTxbnEb5nhbw%3D%3D"></a> Sachinandan</span></div>
+        <div className='p-5 font-thin'>By<span className=' cursor-pointer font-roboto text-blue-400'><a href="https://www.linkedin.com/in/sachinandan-yadav-660115243?"></a> Sachinandan</span></div>
         </div>
         
       </nav>
